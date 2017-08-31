0f29f3cc1ad51f7c7c83480ba81c761a168d2923
Andy Truong <dat13at1@alfa-4.student.lth.se>
2017-08-31 11:08:23 +0200

    tests

--- Andy/lab0/first.py
+++ Andy/lab0/first.py
@@ -29,3 +29,22 @@ print('alphabet[-26]:', alphabet[-26]) # ’a’
 
 print('len(alphabet):', len(alphabet)) # 26
 
+def edits1(word):
+    "All edits that are one edit away from `word`."
+    letters    = 'abcdefghijklmnopqrstuvwxyz'
+    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
+    print(splits)
+    deletes    = [L + R[1:]               for L, R in splits if R]
+    print(deletes)
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
+    print(transposes)
+    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
+    print(replaces)
+    inserts    = [L + c + R               for L, R in splits for c in letters]
+    print(inserts)
+    return set(deletes + transposes + replaces + inserts)
+
+
+def edits2(word):
+    "All edits that are two edits away from `word`."
+    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
