0c0a9fbe497391e70b42f7d139ba77c657a72ca5
Andy Truong <dat13at1@alfa-4.student.lth.se>
2017-08-31 11:42:56 +0200

    unix

--- Andy/lab0/first.py
+++ Andy/lab0/first.py
@@ -48,3 +48,10 @@ def edits1(word):
 def edits2(word):
     "All edits that are two edits away from `word`."
     return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+
+"""
+tr "A-Z" "a-z" < filename #stora bokstäver till små
+tr -cs "A-Za-z" "\n" < filename | sort | uniq -c | sort -nr
+#squeeze och gör ord till \n och sortera, samla och räkna ord, sortera med frekvens
+
+"""
